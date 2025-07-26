94b6940167127e6ae8bd02c78bd0b1d8766401e0
rohithreddyyarabolu93 <rohithreddyyarabolu@gmail.com>
2025-07-26 10:50:35 +0530

    Add files via upload

---
+++ Practical/ControlStructures.java
@@ -0,0 +1,11 @@
+package mypackage;
+public class ControlStructures {
+	public static void main(String[] args) {
+        int temperature = 35;
+        if (temperature > 30) {
+            System.out.println("The temperature is greater than 30");
+        } else {
+            System.out.println("The temperature is less than 30");
+        }
+    }
+}

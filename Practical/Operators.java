e921974e36b5d1805ac87e3030ebe07f9b086cec
rohithreddyyarabolu93 <rohithreddyyarabolu@gmail.com>
2025-07-26 10:42:53 +0530

    Add files via upload

---
+++ Practical/Operators.java
@@ -0,0 +1,13 @@
+package mypackage;
+
+public class Operators {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int sum = a + b;
+        System.out.println("Sum: " + sum); 
+        int difference = a - b;
+        System.out.println("Difference: " + difference); 
+        System.out.println("Is a greater than 'b'? " + (a > b));
+    }
+}

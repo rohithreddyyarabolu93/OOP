44b1920ad7f6b295c0387ef73772c14ffa9f10a5
rohithreddyyarabolu93 <rohithreddyyarabolu@gmail.com>
2025-07-26 10:21:29 +0530

    Add files via upload

---
+++ Practical/Numbers.java
@@ -0,0 +1,11 @@
+package mypackage;
+
+public class Numbers {
+
+	public static void main(String[] args) {        
+        System.out.println("Numbers from 1 to 10:");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}

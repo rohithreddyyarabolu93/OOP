981affea4f6270941df03b906497aaab88db3de7
rohithreddyyarabolu93 <rohithreddyyarabolu@gmail.com>
2025-08-02 11:21:02 +0530

    Add files via upload

---
+++ Practical/TheJavaDateAndTime.java
@@ -0,0 +1,10 @@
+package mypackage;
+import java.time.LocalDateTime;
+public class TheJavaDateAndTime 
+{
+    public static void main(String[] args) 
+    {
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println("Current Date and Time: " + now);
+    }
+}
